8e1496f0455a15300cd370b4effad6669cad5f68
Murilo Medeiros <medeirosmurilo589@gmail.com>
2025-04-24 21:18:40 -0300

    b

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-analytics.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +19,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
-const DB = getFirestore(app);
-export { DB, app };
+const db = getFirestore(app);
+export { db, app };
--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-import { DB } from "./firebaseconfig";
+import { db } from "./firebaseConfig.js";
 import {
   collection,
   addDoc,
-} from "https://gremio-maior-do-brasil-default-rtdb.firebaseio.com/";
+  getDocs,
+  deleteDo,
+  doc,
+} from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
 
+// SALVANDO DADOS NO BANCO
 function getInputs() {
   return {
     nome: document.getElementById("nome"),
@@ -12,10 +16,102 @@ function getInputs() {
   };
 }
 
-function getValues({ nome, idade, cargo }) {
+function getValores({ nome, idade, cargo }) {
   return {
     nome: nome.value.trim(),
     idade: parseInt(idade.value),
     cargo: cargo.value.trim(),
   };
+}
+
+function limpar({ nome, idade, cargo }) {
+  nome.value = "";
+  idade.value = "";
+  cargo.value = "";
+}
+
+document
+  .getElementById("btnEnviar")
+  .addEventListener("click", async function () {
+    const Inputs = getInputs();
+    const dados = getValores(Inputs);
+
+    console.log("Inputs:", Inputs);
+    console.log("Dados", dados);
+
+    if (!dados.idade || !dados.nome || !dados.cargo) {
+      alert("Preencha todos os campos.");
+      return;
+    }
+    try {
+      const ref = await addDoc(collection(db, "funcionarios"), dados);
+      console.log("ID do documento", ref.id);
+      limpar(Inputs);
+      alert("Funcionário cadastrado com sucesso: ");
+    } catch (e) {
+      console.log("Erro: ", e);
+    }
+  });
+
+async function buscarFuncionarios() {
+  const dadosBanco = await getDocs(collection(db, "funcionarios"));
+  console.log(dadosBanco);
+  const funcionarios = [];
+  for (let funcionario of dadosBanco.docs) {
+    funcionarios.push({ id: funcionario.id, ...funcionario.data() });
+  }
+  console.log(funcionarios);
+  return funcionarios;
+}
+
+const listaFuncionariosDiv = document.getElementById("listar-funcionarios");
+
+async function carregarListaDeFuncionarios() {
+  listaFuncionariosDiv.innerHTML =
+    "<p> Carregando lista de Funcionarios... </p>";
+  try {
+    let funcionario = await buscarFuncionarios();
+    console.log(funcionario);
+    renderizarListaDeFuncionarios(funcionario);
+  } catch (error) {
+    console.log("Erro ao carregar a lista de funcionarios: ", error);
+    listaFuncionariosDiv.innerHTML =
+      "<p> Erro ao carregar a lista de funcionários... </p>";
+  }
+}
+
+function renderizarListaDeFuncionarios(funcionarios) {
+  listaFuncionariosDiv.innerHTML = "";
+
+  if (funcionarios.length === 0) {
+    listaFuncionariosDiv.innerHTML =
+      "<p> Nenhum Funcionário cadastrado ainda ;( </p> ";
+    return;
+  }
+  for (let funcionario of funcionarios) {
+    const funcionarioDiv = document.createElement("div");
+    funcionarioDiv.innerHTML = `
+            <strong> Nome: </strong> ${funcionario.nome} <br>
+            <strong> Idade: </strong> ${funcionario.idade} <br>
+            <strong> Cargo: </strong> ${funcionario.cargo} <br>
+            <button class ="bnt-Excluir" data-id="${funcionario.id}"> Excluir </button>
+            <hr>
+        `;
+    listaFuncionariosDiv.appendChild(funcionarioDiv);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", carregarListaDeFuncionarios);
+
+async function excluirfuncionario(idfuncionario) {
+  try {
+    const documentDeletar = doc(db, "funcionarios", idfuncionario);
+    await deleteDoc(documentDeletar);
+    console.log("Funcionario com ID" + idfuncionario + "foi excluido");
+    return true;
+  } catch (erro) {
+    console.log("Erro ao excluir o funcionario", erro);
+    alert("Ocorreu um erro ao excluir funcionario. Tente novamente");
+    return false;
+  }
 }
